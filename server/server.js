e6fa7d65bb42c9b1d41657f46a1505ae66501a4e
Sridhar725S <149698567+Sridhar725S@users.noreply.github.com>
2024-12-14 18:32:30 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require('express-session');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 require('./config/passportConfig');
 
