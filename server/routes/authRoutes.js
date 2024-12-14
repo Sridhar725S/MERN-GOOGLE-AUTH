25950292782441fc53765eae6997343a52f12d20
Sridhar725S <149698567+Sridhar725S@users.noreply.github.com>
2024-12-14 18:43:59 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,9 +5,9 @@ const passport = require('passport');
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 router.get('/google/callback', passport.authenticate('google', {
-    failureRedirect: 'http://localhost:3000',
+    failureRedirect: 'https://mern-google-login.onrender.com',
 }), (req, res) => {
-    res.redirect('http://localhost:3000/profile');
+    res.redirect('https://mern-google-login.onrender.com/profile');
 });
 
 router.get('/current_user', (req, res) => {
@@ -17,7 +17,7 @@ router.get('/current_user', (req, res) => {
 router.get('/logout', (req, res) => {
     req.logout(err => {
         if (err) { return next(err); }
-        res.redirect('http://localhost:3000');
+        res.redirect('https://mern-google-login.onrender.com');
     });
 });
 
