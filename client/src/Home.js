e4e9b68db589d4caef5cc81c04e830e2175e8b73
Sridhar725S <149698567+Sridhar725S@users.noreply.github.com>
2024-12-15 16:04:06 +0530

    Update Home.js

--- client/src/Home.js
+++ client/src/Home.js
@@ -5,8 +5,11 @@ import googleLogo from './assets/google.png'; // Import the Google image
 
 const Login = () => {
     const handleGoogleLogin = () => {
-        // Add your Google login logic here
-        window.open('https://mern-google-login.onrender.com/auth/google', '_self'); // Adjust the URL as necessary
+        // Dynamically handle URL for production and development
+        const googleLoginURL = process.env.NODE_ENV === 'production'
+            ? 'https://mern-google-login.onrender.com/auth/google'
+            : 'http://localhost:5000/auth/google';
+        window.open(googleLoginURL, '_self');
     };
 
     return (
