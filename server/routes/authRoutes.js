7a4a4837d58ee7bb27aea3a5f21b393546d52be3
Sridhar725S <149698567+Sridhar725S@users.noreply.github.com>
2024-12-23 18:55:38 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,22 +2,20 @@
 const router = require('express').Router();
 const passport = require('passport');
 
+// Google authentication route
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
+// Current user route
 router.get('/current_user', (req, res) => {
     res.send(req.user);
 });
 
+// Google callback route
 router.get('/google/callback', passport.authenticate('google', {
     failureRedirect: process.env.NODE_ENV === 'production'
         ? 'https://mern-google-login-client.onrender.com'
         : 'http://localhost:3000',
 }), (req, res) => {
-      // Store only the user ID in the session
-    req.session.userId = req.user.id;
-
-    console.log('User ID stored in session:', req.session.userId);
-    
     if (!req.user) {
         console.error('Authentication failed. req.user is not defined.');
         return res.redirect(process.env.NODE_ENV === 'production'
@@ -25,14 +23,17 @@ router.get('/google/callback', passport.authenticate('google', {
             : 'http://localhost:3000');
     }
 
+    // Store only the user ID in the session
+    req.session.userId = req.user.id;
+    console.log('User ID stored in session:', req.session.userId);
+
     res.redirect(process.env.NODE_ENV === 'production'
         ? 'https://mern-google-login-client.onrender.com/profile'
         : 'http://localhost:3000/profile');
 });
 
-
-
-router.get('/logout', (req, res) => {
+// Logout route
+router.get('/logout', (req, res, next) => {
     req.logout((err) => {
         if (err) return next(err);
         res.redirect(process.env.NODE_ENV === 'production'
