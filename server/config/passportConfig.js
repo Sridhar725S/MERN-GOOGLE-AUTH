0f5af2ed9097617c10a58516d620c99875d1b5b3
Sridhar725S <149698567+Sridhar725S@users.noreply.github.com>
2024-12-27 16:34:46 +0530

    Update passportConfig.js

--- server/config/passportConfig.js
+++ server/config/passportConfig.js
@@ -5,7 +5,7 @@ const User = require('../models/User'); // Your user model
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: process.env.CALLBACK_URL,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
     session: false,  // Disable session support
 }, async (accessToken, refreshToken, profile, done) => {
     try {
