b926c94ee3f716222e9a811efc38795e0742f538
Sridhar725S <149698567+Sridhar725S@users.noreply.github.com>
2024-12-27 13:14:56 +0530

    Update passportConfig.js

--- server/config/passportConfig.js
+++ server/config/passportConfig.js
@@ -11,28 +11,26 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: process.env.NODE_ENV === "production" 
-        ? "https://mern-google-login.onrender.com/auth/google/callback" 
+    callbackURL: process.env.NODE_ENV === "production"
+        ? "https://mern-google-login.onrender.com/auth/google/callback"
         : "http://localhost:5000/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
-    console.log('Google profile:', profile); 
+    console.log('Google profile:', profile);
+
     try {
         let user = await User.findOne({ googleId: profile.id });
 
         if (user) {
-            if (refreshToken) {
-                user.refreshToken = refreshToken;
-                await user.save();
-            }
+            // If user already exists, return the user
             return done(null, user);
         }
 
+        // Create a new user if not found
         user = new User({
             googleId: profile.id,
             username: profile.displayName,
             email: profile.emails[0].value,
             profilePicture: profile.photos[0].value,
-            refreshToken: refreshToken,
         });
 
         await user.save();
