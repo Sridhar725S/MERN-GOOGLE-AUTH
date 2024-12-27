04f716b940b764583fd95bc83c7d989b651e0e1d
Sridhar725S <149698567+Sridhar725S@users.noreply.github.com>
2024-12-27 16:29:57 +0530

    Update passportConfig.js

--- server/config/passportConfig.js
+++ server/config/passportConfig.js
@@ -1,14 +1,12 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
+const User = require('../models/User'); // Your user model
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: process.env.NODE_ENV === 'production'
-        ? "https://mern-google-login.onrender.com/auth/google/callback"
-        : "http://localhost:5000/auth/google/callback"
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    session: false,  // Disable session support
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         let user = await User.findOne({ googleId: profile.id });
@@ -16,29 +14,14 @@ passport.use(new GoogleStrategy({
         if (!user) {
             user = new User({
                 googleId: profile.id,
-                username: profile.displayName,
+                name: profile.displayName,
                 email: profile.emails[0].value,
-                profilePicture: profile.photos[0].value,
-                refreshToken: refreshToken,
             });
             await user.save();
         }
 
-        const token = jwt.sign({ userId: user.id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1d' });
-        return done(null, { user, token });
-
-    } catch (err) {
-        console.error('Error authenticating user:', err);
-        return done(err, null);
+        done(null, { user, token: accessToken }); // You can create a JWT here if needed
+    } catch (error) {
+        done(error, false);
     }
 }));
-
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => {
-        done(err, user);
-    });
-});
